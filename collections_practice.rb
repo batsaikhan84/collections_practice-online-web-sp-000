5fe7674b07fcb4e93c6a9a6a05363283e07f7dcb
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-14 23:24:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,5 +7,17 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort {|a,b| a<=>b}
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array, 1, 2)
+end
+
+def (reverse_array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  
 end
